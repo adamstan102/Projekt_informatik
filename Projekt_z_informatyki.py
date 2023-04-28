ab748f907e13d93a798c37e3b6cb8e3d7585db24
adamstan102 <129062363+adamstan102@users.noreply.github.com>
2023-04-28 19:27:16 +0200

    Update Projekt_z_informatyki.py

--- Projekt_z_informatyki.py
+++ Projekt_z_informatyki.py
@@ -10,3 +10,13 @@ import numpy as np
 from numpy import rad2deg, deg2rad, floor, array, append, linalg, dot, arccos
 import argparse
 
+
+        if model == "wgs84":
+            self.a = 6378137.0 # semimajor_axis
+            self.b = 6356752.31424518 # semiminor_axis
+        elif model == "grs80":
+            self.a = 6378137.0
+            self.b = 6356752.31414036
+        elif model == "Krasowski":
+            self.a = 6378245.0
+            self.b = 6356863.01877
