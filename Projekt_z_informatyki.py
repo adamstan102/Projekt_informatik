e693684563656ee35e8e4db153bac0985756ac70
adamstan102 <129062363+adamstan102@users.noreply.github.com>
2023-04-28 19:18:44 +0200

    Update Projekt_z_informatyki.py

--- Projekt_z_informatyki.py
+++ Projekt_z_informatyki.py
@@ -5,16 +5,8 @@ Created on Fri Apr 28 14:31:01 2023
 @author: User
 """
 
-aaa
+from math import sin, cos, sqrt, atan, atan2, degrees, radians, pi, tan
+import numpy as np
+from numpy import rad2deg, deg2rad, floor, array, append, linalg, dot, arccos
+import argparse
 
-assaas
-nic nie pisz bo nie wolmo
-nie ładnie tak
-halo halo!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-i jak tam idzie coś?
-
-
-tak se
-
-no to bardzo dobrze 
